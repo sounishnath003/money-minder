39d85fc877562cbefc87a06d6320e5ef702a7153
Sounish Nath <flock.sinasini@gmail.com>
2025-05-22 01:12:32 +0530

    Refactor grid layout in Home.vue to enhance spacing and improve overall design consistency.

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="grid grid-rows-2 lg:grid-cols-2 gap-2 md:gap-8 md:gap-20 my-6 mx-3">
-        <AddTransaction />
+    <div class="grid grid-rows-2 lg:grid-cols-2 gap-20 my-6 mx-3">
+        <AddTransaction/>
         <Balance class="flex flex-col justify-start items-center order-first lg:order-last" />
     </div>
 </template>
