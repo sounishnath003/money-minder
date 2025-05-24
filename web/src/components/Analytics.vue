8494785b50da6d437da7b2b1949d7107a49f125a
Sounish Nath <flock.sinasini@gmail.com>
2025-05-24 12:02:46 +0530

    Enhance analytics functionality by integrating ApexCharts into Analytics.vue for visual data representation. Update tsconfig.json to include type definitions for ApexCharts and VueApexCharts. Modify main.ts to register VueApexCharts in the Vue application.

--- web/src/components/Analytics.vue
+++ web/src/components/Analytics.vue
@@ -1,3 +1,83 @@
 <template>
     <div class="text-3xl">Analytics Section</div>
+
+    <div id="chart">
+        <apexchart type="area" height="350" :options="chartOptions" :series="series"></apexchart>
+    </div>
 </template>
+
+<script setup>
+
+const dates = [
+    [1716537600000, 10],
+    [1716624000000, 20],
+    [1716710400000, 30],
+    [1716796800000, 40],
+    [1716883200000, 50],
+    [1716969600000, 60],
+    [1717056000000, 70],
+]
+const series = [{
+    name: 'XYZ MOTORS',
+    data: dates
+}];
+
+
+const chartOptions = {
+    chart: {
+        type: 'area',
+        stacked: false,
+        height: 350,
+        zoom: {
+            type: 'x',
+            enabled: true,
+            autoScaleYaxis: true
+        },
+        toolbar: {
+            autoSelected: 'zoom'
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    markers: {
+        size: 0,
+    },
+    title: {
+        text: 'Stock Price Movement',
+        align: 'left'
+    },
+    fill: {
+        type: 'gradient',
+        gradient: {
+            shadeIntensity: 1,
+            inverseColors: false,
+            opacityFrom: 0.5,
+            opacityTo: 0,
+            stops: [0, 90, 100]
+        },
+    },
+    yaxis: {
+        labels: {
+            formatter: function (val) {
+                return val.toFixed(0); // (val / 1000000).toFixed(0);
+            },
+        },
+        title: {
+            text: 'Price'
+        },
+    },
+    xaxis: {
+        type: 'datetime',
+    },
+    tooltip: {
+        shared: false,
+        y: {
+            formatter: function (val) {
+                return (val / 1000000).toFixed(0)
+            }
+        }
+    }
+};
+
+</script>
--- web/src/main.ts
+++ web/src/main.ts
@@ -4,7 +4,14 @@ import App from './App.vue'
 import { router } from './router'
 import { createPinia } from 'pinia'
 
+import VueApexCharts from "vue3-apexcharts";
+
 const app = createApp(App)
+// Register a central store for the vue application
 app.use(createPinia());
+// Register the routes into the application
 app.use(router);
+// Define the apex charts into the vue components
+app.use(VueApexCharts);
+// Mount the application with the DOM #id
 app.mount('#app');
